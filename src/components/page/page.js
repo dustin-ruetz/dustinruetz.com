3a975d5e10ff74fbff65b4afe65dff10393bcdda
Dustin Ruetz <dustin.ruetz@telus.com>
2020-07-27 11:27:16 -0400

    feat: add header component, update favicon

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -3,6 +3,7 @@ require('./reset.scss')
 // styles needed on every page
 require('./page.scss')
 // styles needed for components that appear on every page
+require('../header/header.scss')
 require('../heading/heading.scss')
 require('../img/img.scss')
 require('../nav/nav.scss')
