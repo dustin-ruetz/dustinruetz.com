e5aa0796e2928dfa2299da64abd27061aca591a4
Dustin Ruetz <dustin.ruetz@telus.com>
2021-02-12 15:48:52 -0500

    fix(pages): remove console.log() messages from scripts

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -3,6 +3,3 @@ require('../../components/index.scss')
 require('./404.scss')
 
 require('./images/you-are-being-monitored.png')
-
-// eslint-disable-next-line no-console
-console.log('This is 404.js')
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,3 @@
 require('../../components/index.js')
 require('../../components/index.scss')
 require('./home.scss')
-
-// eslint-disable-next-line no-console
-console.log('This is home.js')
