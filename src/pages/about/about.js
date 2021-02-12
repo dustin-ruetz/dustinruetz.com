127145b3860111b4a0e4178727a5477030597b1e
Dustin Ruetz <dustin.ruetz@telus.com>
2021-02-12 15:54:54 -0500

    refactor(pages/about): rename featured image

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,4 +1,4 @@
 require('../../components/index.js')
 require('../../components/index.scss')
 require('./about.scss')
-require('./images/digital-ceo-2250x2250-grayscale.jpg')
+require('./images/digital-ceo-award-2250x2250-grayscale.jpg')
