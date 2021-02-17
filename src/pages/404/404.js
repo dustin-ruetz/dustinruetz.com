650404f5bd1212b2f0cc4d23db1ac22aa77bd98a
Dustin Ruetz <dustin.ruetz@telus.com>
2021-02-16 21:00:48 -0500

    fix(pages): require images properly so that they're processed by Webpack

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,5 +1,3 @@
 require('../../components/index.js')
 require('../../components/index.scss')
 require('./404.scss')
-
-require('./images/you-are-being-monitored.png')
--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,4 +1,3 @@
 require('../../components/index.js')
 require('../../components/index.scss')
 require('./about.scss')
-require('./images/digital-ceo-award-2250x2250-grayscale.jpg')
