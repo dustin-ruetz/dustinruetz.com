411211e1d772a82149c2d00bb6c6076285bf9999
Dustin Ruetz <dustin.ruetz@telus.com>
2021-02-17 17:07:24 -0500

    feat(pages/404): initial 404 page

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,3 +1,14 @@
 require('../../components/index.js')
 require('../../components/index.scss')
 require('./404.scss')
+
+document.addEventListener('DOMContentLoaded', () => {
+  const error404description = document.querySelector('.error-404__description')
+
+  const descriptionWithRequestedPage = error404description.textContent.replace(
+    '{requestedPage}',
+    `<span class="error-404__requested-page">${window.location.pathname}</span>`,
+  )
+
+  error404description.innerHTML = descriptionWithRequestedPage
+})
