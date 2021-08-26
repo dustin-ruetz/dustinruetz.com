51c001437a60bd16d4fcfd3173f57d314152e9ea
Dustin Ruetz <dustin.ruetz@telus.com>
2021-08-26 11:29:06 -0400

    docs(config/webpack): clarify comments for host and https options

--- config/webpack.dev-local.js
+++ config/webpack.dev-local.js
@@ -5,11 +5,12 @@ const webpackConfigCommon = require('./webpack.common.js')
 const webpackConfigDevelopment = require('./webpack.development.js')
 
 module.exports = webpackMerge(webpackConfigCommon, webpackConfigDevelopment, {
+  // https://webpack.js.org/configuration/dev-server/
   devServer: {
     // specify local development host
-    // note: port is determined automatically
+    // note: server will automatically find a free port to use
     host: `${URL.subdomain.local}.${URL.domain.local}`,
-    // define certicate and key to use HTTPS in local development
+    // define certicate and key to use HTTPS for local clients
     https: {
       cert: readFileSync('./ssl/public.crt'),
       key: readFileSync('./ssl/private.key'),
--- config/webpack.dev-network.js
+++ config/webpack.dev-network.js
@@ -3,9 +3,11 @@ const webpackConfigCommon = require('./webpack.common.js')
 const webpackConfigDevelopment = require('./webpack.development.js')
 
 module.exports = webpackMerge(webpackConfigCommon, webpackConfigDevelopment, {
+  // https://webpack.js.org/configuration/dev-server/
   devServer: {
-    // specify host as 'local-ip' to try and resolve the local IPv4 address
-    // note: port is determined automatically
+    // specify host as 'local-ip' to try to resolve the local IPv4 address, if available;
+    // if local IPv4 address is unavailable then try to resolve the local IPv6 address
+    // note: server will automatically find a free port to use
     host: 'local-ip',
     // use a self-signed certificate to use HTTPS for network clients
     https: true,
