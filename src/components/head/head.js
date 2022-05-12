b2861934449f406fe60b385ce8609a135ceaa189
Dustin Ruetz <dtj.ruetz@gmail.com>
2022-05-11 21:00:41 -0400

    refactor(components/head): require favicon directly in Pug mixin file instead of JS file

--- src/components/head/head.js
+++
@@ -1,1 +0,0 @@
-require('./favicons/favicon-blue-180x180.png')
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
-require('./head/head.js')
+// this file (./src/components/index.js) should require:
+// 1. styles needed on every page
+// 2. styles needed for components that appear on every page
